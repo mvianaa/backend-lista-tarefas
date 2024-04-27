11ab3f9e9947bc9269c71bf58264550c2a278b8b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-26 22:31:35 -0300

    adicionando nova tarefa

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000; // Defina a porta do servidor
+
+app.use(express.json()); // Responsável por fazer o parse do corpo das requisições HTTP com formato JSON
+
+let tarefas = [];
+
+// Rota para adicionar uma nova tarefa
+app.post('/tarefas', (req, res) => {
+    const { tarefa } = req.body;
+    tarefas.push({  id:tarefas.length + 1, tarefa });
+    res.status(201).json({ message: 'Tarefa adicionada com sucesso!' });
+});
+
+// Rota para obter todas as tarefas
+app.get('/tarefas/:id', (req, res) => {
+    // Lógica para obter uma tarefas do banco de dados pelo seu ID
+});
+
+// Rota para obter uma tarefas específica
+app.get('/tarefas', (req, res) => {
+    // Lógica para obter e retornar todas as tarefas do banco de dados
+});
+
+// Rota para editar uma tarefa existente
+app.put('/tarefas/:id', (req, res) => {
+    // Lógica para editar uma tarefa no banco de dados com base no ID
+});
+
+// Rota para excluir uma tarefa
+app.delete('/tarefas/:id', (req, res) => {
+    // Lógica para excluir uma tarefa do banco de dados com base no ID
+});
+// Inicie o servidor Express:
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta em http;//localhost:${PORT}`);
+});
